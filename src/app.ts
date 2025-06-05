4802143e98973852f9b906dd57e6e9f7ef335f01
lva100 <lva100@yandex.ru>
2025-06-05 16:06:29 +0300

    Final commit

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,9 @@
-import { PromptService } from './core/prompt/prompt.service'
+import { DirExecuter } from './dir/dir.executor'
+import { ConsoleLogger } from './out/console-logger/console-logger'
 
 export class App {
 	async run() {
-		const res1 = await new PromptService().input('Число:', 'number')
-		console.log(res1)
-		const res2 = await new PromptService().input('Слово:', 'input')
-		console.log(res2)
-		const res3 = await new PromptService().input('Пароль:', 'password')
-		console.log(res3)
+		new DirExecuter(ConsoleLogger.getInstance()).execute()
 	}
 }
 
